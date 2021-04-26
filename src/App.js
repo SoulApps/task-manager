ad0ae1242785ff904971f6fc6cd57391a43da822
Ramón Guardia López <ramon.guardialopez@gmail.com>
2021-04-26 20:31:57 +0200

    v0.1
    Creación primeros componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 
 function App() {
+  
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true, 
+    },
+    {
+        id: 2,
+        text: 'FRS Meeting',
+        day: 'Feb 6th at 2:30pm',
+        reminder: false, 
+    },
+    {
+        id: 3,
+        text: 'IT Meeting',
+        day: 'Feb 6th at 10am',
+        reminder: true, 
+    },
+    {
+        id: 4,
+        text: 'Sports Event',
+        day: 'Feb 10th at 5pm',
+        reminder: false, 
+    },
+    {
+        id: 5,
+        text: 'Doctors 2nd Appointment',
+        day: 'Feb 11th at 2:30pm',
+        reminder: true, 
+    },
+    {
+        id: 6,
+        text: 'B Day',
+        day: 'Feb 22nd at 11:30am',
+        reminder: true, 
+    },
+
+    ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
